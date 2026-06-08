eaced734305e161ee63dcc4d8bacd687beb01a12
A01753660 <a01753660@tec.mx>
2026-06-07 20:38:16 -0600

    Add files via upload

--- Pelicula.cpp
+++ Pelicula.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Pelicula.hpp"
+
+Pelicula::Pelicula() : Video() {}
+Pelicula::Pelicula(string id, string nombre , int duracionMinutos, string genero)
+      : Video(id, nombre, duracionMinutos, genero) {}
+
+string Pelicula::mostrarInfo() {
+    string resultado = "Pelicula: " + getNombre() + "\n" + "Genero: " + getGenero() + "\n" + "Clasificación Promedio: " + to_string(getPromedio()) + "\n";
+    return resultado;
+}
